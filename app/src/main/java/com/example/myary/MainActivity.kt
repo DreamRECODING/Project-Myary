c4fbd172588e5c98659622614c4382cca66a836c
Kitsune2002 <jeongch3@naver.com>
2022-11-18 19:58:35 +0900

    Commit Message

--- app/src/main/java/com/example/myary/MainActivity.kt
+++ app/src/main/java/com/example/myary/MainActivity.kt
@@ -3,10 +3,43 @@ package com.example.myary
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.myary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        replaceFragment(Mytory())
+
+        binding.bottomNavigationView.setOnItemReselectedListener{
+
+            when(it.itemId){
+
+                R.id.mytory -> replaceFragment(Mytory())
+                R.id.mychive -> replaceFragment(Mychive())
+                R.id.taskchaser -> replaceFragment(Task_Chaser())
+                else ->{
+
+                }
+
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTranscation = fragmentManager.beginTransaction()
+        fragmentTranscation.replace(R.id.frame_layout, fragment)
+        fragmentTranscation.commit()
+
+
     }
 }
