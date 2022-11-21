754a0dc82ee207ad4fc4fab2751841ffd2939380
ywnsl14 <66462574+ywnsl14@users.noreply.github.com>
2022-11-21 13:47:39 +0900

    Update Mychive.kt

--- app/src/main/java/com/example/myary/Mychive.kt
+++ app/src/main/java/com/example/myary/Mychive.kt
@@ -1,59 +1,158 @@
 package com.example.myary
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Context.MODE_NO_LOCALIZED_COLLATORS
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 import android.view.View
+import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import android.view.ViewGroup
+import android.view.LayoutInflater
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Mychive.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Mychive : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    //var userID: String = "userID"
+    private lateinit var fname: String
+    private lateinit var str: String
+    private lateinit var calendarView: CalendarView
+    private lateinit var updateBtn: Button
+    private lateinit var deleteBtn:Button
+    private lateinit var saveBtn:Button
+    private lateinit var diaryTextView: TextView
+    private lateinit var diaryContent:TextView
+    private lateinit var title:TextView
+    private lateinit var contextEditText: EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_mychive, container, false)
+
+    }
+
+
+    @SuppressLint("MissingInflatedId")
+    override fun onStart() {
+        super.onStart()
+
+        // UI값 생성
+        calendarView= requireView().findViewById(R.id.calendarView)
+        diaryTextView = requireView().findViewById(R.id.diaryTextView)
+        saveBtn=requireView().findViewById(R.id.saveBtn)
+        deleteBtn=requireView().findViewById(R.id.deleteBtn)
+        updateBtn=requireView().findViewById(R.id.updateBtn)
+        diaryContent=requireView().findViewById(R.id.diaryContent)
+        title=requireView().findViewById(R.id.title)
+        contextEditText=requireView().findViewById(R.id.contextEditText)
+
+        title.text = "Mychive"
+
+        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            diaryTextView.visibility = View.VISIBLE
+            saveBtn.visibility = View.VISIBLE
+            contextEditText.visibility = View.VISIBLE
+            diaryContent.visibility = View.INVISIBLE
+            updateBtn.visibility = View.INVISIBLE
+            deleteBtn.visibility = View.INVISIBLE
+            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)
+            contextEditText.setText("")
+            checkDay(year, month, dayOfMonth)
+        }
+
+        saveBtn.setOnClickListener {
+            saveDiary(fname)
+            contextEditText.visibility = View.INVISIBLE
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            str = contextEditText.text.toString()
+            diaryContent.text = str
+            diaryContent.visibility = View.VISIBLE
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mychive, container, false)
+    // 달력 내용 조회, 수정
+    fun checkDay(cYear: Int, cMonth: Int, cDay: Int) {
+        //저장할 파일 이름설정
+        fname = "" + cYear + "-" + (cMonth + 1) + "" + "-" + cDay + ".txt"
+
+        var fileInputStream: FileInputStream
+        try {
+            fileInputStream = requireActivity().openFileInput(fname)
+            val fileData = ByteArray(fileInputStream.available())
+            fileInputStream.read(fileData)
+            fileInputStream.close()
+            str = String(fileData)
+            contextEditText.visibility = View.INVISIBLE
+            diaryContent.visibility = View.VISIBLE
+            diaryContent.text = "${str}"
+            saveBtn.visibility = View.INVISIBLE
+            updateBtn.visibility = View.VISIBLE
+            deleteBtn.visibility = View.VISIBLE
+            updateBtn.setOnClickListener {
+                contextEditText.visibility = View.VISIBLE
+                diaryContent.visibility = View.INVISIBLE
+                contextEditText.setText(str)
+                saveBtn.visibility = View.VISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryContent.text = contextEditText.text
+            }
+            deleteBtn.setOnClickListener {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                contextEditText.setText("")
+                contextEditText.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                removeDiary(fname)
+            }
+            if (diaryContent.text == null) {
+                diaryContent.visibility = View.INVISIBLE
+                updateBtn.visibility = View.INVISIBLE
+                deleteBtn.visibility = View.INVISIBLE
+                diaryTextView.visibility = View.VISIBLE
+                saveBtn.visibility = View.VISIBLE
+                contextEditText.visibility = View.VISIBLE
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Mychive.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Mychive().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+    // 달력 내용 제거
+    @SuppressLint("WrongConstant")
+    fun removeDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = requireActivity().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = ""
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    // 달력 내용 추가
+    @SuppressLint("WrongConstant")
+    fun saveDiary(readDay: String?) {
+        var fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = requireActivity().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
+            val content = contextEditText.text.toString()
+            fileOutputStream.write(content.toByteArray())
+            fileOutputStream.close()
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
     }
 }
