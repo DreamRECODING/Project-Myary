4eaec5089b7d3bb20737058cc0709dd80ef18d19
ywnsl14 <66462574+ywnsl14@users.noreply.github.com>
2022-11-26 22:47:30 +0900

    Update Mychive.kt

--- app/src/main/java/com/example/myary/Mychive.kt
+++ app/src/main/java/com/example/myary/Mychive.kt
@@ -16,23 +16,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import com.example.myary.databinding.FragmentMychiveBinding
 
 
 class Mychive : Fragment() {
 
     private lateinit var fname: String
     private lateinit var str: String
-    private lateinit var calendarView: CalendarView
-    private lateinit var updateBtn: Button
-    private lateinit var deleteBtn:Button
-    private lateinit var saveBtn:Button
-    private lateinit var diaryTextView: TextView //선택한 날짜 보여주는 TextVew
-    private lateinit var diaryContent:TextView  //일정, 다이어리 내용 보여주는 TextView
-    private lateinit var title:TextView
-    private lateinit var contextEditText: EditText
+    
+    private lateinit var binding: FragmentMychiveBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.fragment_mychive, container, false)
+        binding = FragmentMychiveBinding.inflate(inflater, container, false)
+        return binding.root
 
     }
 
@@ -40,41 +36,31 @@ class Mychive : Fragment() {
     @SuppressLint("MissingInflatedId")
     override fun onStart() {
         super.onStart()
+       
+        binding.title.text = "Mychive"
 
-        // UI값 생성
-        calendarView= requireView().findViewById(R.id.calendarView)
-        diaryTextView = requireView().findViewById(R.id.diaryTextView)
-        saveBtn=requireView().findViewById(R.id.saveBtn)
-        deleteBtn=requireView().findViewById(R.id.deleteBtn)
-        updateBtn=requireView().findViewById(R.id.updateBtn)
-        diaryContent=requireView().findViewById(R.id.diaryContent)
-        title=requireView().findViewById(R.id.title)
-        contextEditText=requireView().findViewById(R.id.contextEditText)
-
-        title.text = "Mychive"
-
-        calendarView.setOnDateChangeListener { view, year, month, dayOfMonth -> //달력 날짜 선택
-            diaryTextView.visibility = View.VISIBLE    //날짜 뜸
-            saveBtn.visibility = View.VISIBLE   //저장 버튼 뜸
-            contextEditText.visibility = View.VISIBLE   //일정 입력하는 EditText 뜸
-            diaryContent.visibility = View.INVISIBLE    //저장된 일정 내용 textView 안 보이게
-            updateBtn.visibility = View.INVISIBLE       //수정 버튼 안 보임
-            deleteBtn.visibility = View.INVISIBLE       //삭제 버튼 안 보임
-            diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)     //날짜 뜨는 textView에 날짜 뜸
-            contextEditText.setText("")     //EditText 공백
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth -> //달력 날짜 선택
+            binding.diaryTextView.visibility = View.VISIBLE    //날짜 뜸
+            binding.saveBtn.visibility = View.VISIBLE   //저장 버튼 뜸
+            binding.contextEditText.visibility = View.VISIBLE   //일정 입력하는 EditText 뜸
+            binding.diaryContent.visibility = View.INVISIBLE    //저장된 일정 내용 textView 안 보이게
+            binding.updateBtn.visibility = View.INVISIBLE       //수정 버튼 안 보임
+            binding.deleteBtn.visibility = View.INVISIBLE       //삭제 버튼 안 보임
+            binding.diaryTextView.text = String.format("%d / %d / %d", year, month + 1, dayOfMonth)     //날짜 뜨는 textView에 날짜 뜸
+            binding.contextEditText.setText("")     //EditText 공백
             checkDay(year, month, dayOfMonth)       //checkDay 호출 (달력 내용 조회, 수정)
         }
 
         //저장 버튼 누름
-        saveBtn.setOnClickListener {
+        binding.saveBtn.setOnClickListener {
             saveDiary(fname) //saveDiary 호출
-            contextEditText.visibility = View.INVISIBLE
-            saveBtn.visibility = View.INVISIBLE
-            updateBtn.visibility = View.VISIBLE
-            deleteBtn.visibility = View.VISIBLE
-            str = contextEditText.text.toString() //str 변수에 EditText 내용을 string으로 저장
-            diaryContent.text = str
-            diaryContent.visibility = View.VISIBLE  //일정 내용 보임
+            binding.contextEditText.visibility = View.INVISIBLE
+            binding.saveBtn.visibility = View.INVISIBLE
+            binding.updateBtn.visibility = View.VISIBLE
+            binding.deleteBtn.visibility = View.VISIBLE
+            str = binding.contextEditText.text.toString() //str 변수에 EditText 내용을 string으로 저장
+            binding.diaryContent.text = str
+            binding.diaryContent.visibility = View.VISIBLE  //일정 내용 보임
         }
     }
 
@@ -90,43 +76,43 @@ class Mychive : Fragment() {
             fileInputStream.read(fileData)  //fileData 읽어오기 fileInputStream
             fileInputStream.close() //close 후 파일에 접근 가능
             str = String(fileData)  //str 변수에 fileData를 string으로 저장
-            contextEditText.visibility = View.INVISIBLE     //이때 수정하는 EditText 칸은 안 보임
-            diaryContent.visibility = View.VISIBLE      //입력한 일정 내용 칸 보임
-            diaryContent.text = "${str}"    //EditText로 입력한 일정 내용(str) 출력
-            saveBtn.visibility = View.INVISIBLE
-            updateBtn.visibility = View.VISIBLE
-            deleteBtn.visibility = View.VISIBLE         //저장 버튼은 안 보이고, 수정과 삭제 버튼이 보임
+            binding.contextEditText.visibility = View.INVISIBLE     //이때 수정하는 EditText 칸은 안 보임
+            binding.diaryContent.visibility = View.VISIBLE      //입력한 일정 내용 칸 보임
+            binding.diaryContent.text = "${str}"    //EditText로 입력한 일정 내용(str) 출력
+            binding.saveBtn.visibility = View.INVISIBLE
+            binding.updateBtn.visibility = View.VISIBLE
+            binding.deleteBtn.visibility = View.VISIBLE         //저장 버튼은 안 보이고, 수정과 삭제 버튼이 보임
             
             //수정버튼 누름
-            updateBtn.setOnClickListener {
-                contextEditText.visibility = View.VISIBLE       //EditText 칸이 보임
-                diaryContent.visibility = View.INVISIBLE        //내용 보이던 TextView는 안 보임, 수정할 거니까
-                contextEditText.setText(str)        //editText에 TextView에 저장됐던 내용 출력, 수정 위해
-                saveBtn.visibility = View.VISIBLE   
-                updateBtn.visibility = View.INVISIBLE
-                deleteBtn.visibility = View.INVISIBLE       //저장 버튼 보이게, 나머지 버튼은 안 보이게
-                diaryContent.text = contextEditText.text    //내용 보이는 TextView에 EditText로 작성한 내용 저장
+            binding.updateBtn.setOnClickListener {
+                binding.contextEditText.visibility = View.VISIBLE       //EditText 칸이 보임
+                binding.diaryContent.visibility = View.INVISIBLE        //내용 보이던 TextView는 안 보임, 수정할 거니까
+                binding.contextEditText.setText(str)        //editText에 TextView에 저장됐던 내용 출력, 수정 위해
+                binding.saveBtn.visibility = View.VISIBLE
+                binding.updateBtn.visibility = View.INVISIBLE
+                binding.deleteBtn.visibility = View.INVISIBLE       //저장 버튼 보이게, 나머지 버튼은 안 보이게
+                binding.diaryContent.text = binding.contextEditText.text    //내용 보이는 TextView에 EditText로 작성한 내용 저장
             }
             
             //삭제버튼 누름
-            deleteBtn.setOnClickListener {
-                diaryContent.visibility = View.INVISIBLE
-                updateBtn.visibility = View.INVISIBLE
-                deleteBtn.visibility = View.INVISIBLE
-                contextEditText.setText("")     //EditText 부분이 공백으로 바뀜
-                contextEditText.visibility = View.VISIBLE       //EditText는 보이게, 내용 저장되어 보여주던 TextView(diaryContent)는 안 보이게
-                saveBtn.visibility = View.VISIBLE       //처음 화면처럼 내용 없으니까 저장 버튼 보이게
+            binding.deleteBtn.setOnClickListener {
+                binding.diaryContent.visibility = View.INVISIBLE
+                binding.updateBtn.visibility = View.INVISIBLE
+                binding.deleteBtn.visibility = View.INVISIBLE
+                binding.contextEditText.setText("")     //EditText 부분이 공백으로 바뀜
+                binding.contextEditText.visibility = View.VISIBLE       //EditText는 보이게, 내용 저장되어 보여주던 TextView(diaryContent)는 안 보이게
+                binding.saveBtn.visibility = View.VISIBLE       //처음 화면처럼 내용 없으니까 저장 버튼 보이게
                 removeDiary(fname)      //removeDiary 호출
             }
             
             //diaryContent(TextView)에 아무것도 없다면
-            if (diaryContent.text == null) {
-                diaryContent.visibility = View.INVISIBLE
-                updateBtn.visibility = View.INVISIBLE
-                deleteBtn.visibility = View.INVISIBLE
-                diaryTextView.visibility = View.VISIBLE     //날짜는 보여주고
-                saveBtn.visibility = View.VISIBLE
-                contextEditText.visibility = View.VISIBLE       //내용 입력할 수 있게 EditText와 저장버튼 뜨게 함
+            if (binding.diaryContent.text == null) {
+                binding.diaryContent.visibility = View.INVISIBLE
+                binding.updateBtn.visibility = View.INVISIBLE
+                binding.deleteBtn.visibility = View.INVISIBLE
+                binding.diaryTextView.visibility = View.VISIBLE     //날짜는 보여주고
+                binding.saveBtn.visibility = View.VISIBLE
+                binding.contextEditText.visibility = View.VISIBLE       //내용 입력할 수 있게 EditText와 저장버튼 뜨게 함
             }
         } catch (e: Exception) {
             e.printStackTrace()     //에러 발생하면 발생 이유와 위치 로그에 뜨게 함
@@ -156,7 +142,7 @@ class Mychive : Fragment() {
         var fileOutputStream: FileOutputStream //파일 생성
         try {
             fileOutputStream = requireActivity().openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS)
-            val content = contextEditText.text.toString() //EditText의 내용을 string으로, content에 저장
+            val content = binding.contextEditText.text.toString() //EditText의 내용을 string으로, content에 저장
             fileOutputStream.write(content.toByteArray())   //파일 작성
             fileOutputStream.close()
         } catch (e: java.lang.Exception) {
