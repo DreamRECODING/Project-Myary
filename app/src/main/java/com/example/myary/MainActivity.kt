87bef23ec8664d196950524a13e206aac4d8b1f0
Kitsune2002 <jeongch3@naver.com>
2022-11-28 16:36:08 +0900

    Commit Message

--- app/src/main/java/com/example/myary/MainActivity.kt
+++ app/src/main/java/com/example/myary/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.myary
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.myary.databinding.ActivityMainBinding
+import com.example.myary.Mychive
+import com.example.myary.taskchaser.Task_Chaser
+import com.example.myary.Mytory
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
         replaceFragment(Mytory())
 
-        binding.bottomNavigationView.setOnItemReselectedListener{
+        binding.bottomNavigationView.setOnItemSelectedListener{
 
             when(it.itemId){
 
